fd3d2875c3da719c43f9553083e860d206b68822
jlegostaev <j.legostaev@gmail.com>
2015-12-02 16:55:26 +0200

    protected api

--- server/routes/crud.js
+++ server/routes/crud.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
+var isAuthenticated = function (req, res, next) {
+  // if user is authenticated in the session, call the next() to call the next request handler
+  // Passport adds this method to request object. A middleware is allowed to add properties to
+  // request and response objects
+  if (req.isAuthenticated())
+    return next();
+  // if the user is not authenticated then redirect him to the login page
+  return res.status(401).end();
+}
+
 // get all goods
-router.get('/', function(req, res) {
+router.get('/', isAuthenticated, function(req, res) {
   console.log('/api/goods');
   console.log('isAuthenticated'+req.isAuthenticated());
   console.log(req.user);
